caced968d49b6810fd6c2122721e0d208a1457d3
htsgts <davidmoon824@naver.com>
2024-09-10 16:18:51 +0900

    3주차 실습

---
+++ 03rd/sln_03/A_RBMsg/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace A_RBMsg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbOption1 = new System.Windows.Forms.GroupBox();
+            this.rbOk = new System.Windows.Forms.RadioButton();
+            this.rbOkCancel = new System.Windows.Forms.RadioButton();
+            this.rbYesNo = new System.Windows.Forms.RadioButton();
+            this.gbOption2 = new System.Windows.Forms.GroupBox();
+            this.rbQuestion = new System.Windows.Forms.RadioButton();
+            this.rbInformation = new System.Windows.Forms.RadioButton();
+            this.rbError = new System.Windows.Forms.RadioButton();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.gbOption1.SuspendLayout();
+            this.gbOption2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbOption1
+            // 
+            this.gbOption1.Controls.Add(this.rbOk);
+            this.gbOption1.Controls.Add(this.rbOkCancel);
+            this.gbOption1.Controls.Add(this.rbYesNo);
+            this.gbOption1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gbOption1.Location = new System.Drawing.Point(33, 68);
+            this.gbOption1.Name = "gbOption1";
+            this.gbOption1.Size = new System.Drawing.Size(184, 212);
+            this.gbOption1.TabIndex = 0;
+            this.gbOption1.TabStop = false;
+            this.gbOption1.Text = "Type";
+            // 
+            // rbOk
+            // 
+            this.rbOk.Location = new System.Drawing.Point(25, 56);
+            this.rbOk.Name = "rbOk";
+            this.rbOk.Size = new System.Drawing.Size(108, 21);
+            this.rbOk.TabIndex = 2;
+            this.rbOk.TabStop = true;
+            this.rbOk.Text = "OK";
+            this.rbOk.UseVisualStyleBackColor = true;
+            // 
+            // rbOkCancel
+            // 
+            this.rbOkCancel.AutoSize = true;
+            this.rbOkCancel.Location = new System.Drawing.Point(25, 105);
+            this.rbOkCancel.Name = "rbOkCancel";
+            this.rbOkCancel.Size = new System.Drawing.Size(84, 21);
+            this.rbOkCancel.TabIndex = 3;
+            this.rbOkCancel.TabStop = true;
+            this.rbOkCancel.Text = "OKCancel";
+            this.rbOkCancel.UseVisualStyleBackColor = true;
+            // 
+            // rbYesNo
+            // 
+            this.rbYesNo.AutoSize = true;
+            this.rbYesNo.Location = new System.Drawing.Point(25, 156);
+            this.rbYesNo.Name = "rbYesNo";
+            this.rbYesNo.Size = new System.Drawing.Size(65, 21);
+            this.rbYesNo.TabIndex = 4;
+            this.rbYesNo.TabStop = true;
+            this.rbYesNo.Text = "YesNo";
+            this.rbYesNo.UseVisualStyleBackColor = true;
+            // 
+            // gbOption2
+            // 
+            this.gbOption2.Controls.Add(this.rbQuestion);
+            this.gbOption2.Controls.Add(this.rbInformation);
+            this.gbOption2.Controls.Add(this.rbError);
+            this.gbOption2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gbOption2.Location = new System.Drawing.Point(290, 68);
+            this.gbOption2.Name = "gbOption2";
+            this.gbOption2.Size = new System.Drawing.Size(184, 212);
+            this.gbOption2.TabIndex = 1;
+            this.gbOption2.TabStop = false;
+            this.gbOption2.Text = "Icon";
+            // 
+            // rbQuestion
+            // 
+            this.rbQuestion.AutoSize = true;
+            this.rbQuestion.Location = new System.Drawing.Point(26, 105);
+            this.rbQuestion.Name = "rbQuestion";
+            this.rbQuestion.Size = new System.Drawing.Size(82, 21);
+            this.rbQuestion.TabIndex = 2;
+            this.rbQuestion.TabStop = true;
+            this.rbQuestion.Text = "Question";
+            this.rbQuestion.UseVisualStyleBackColor = true;
+            // 
+            // rbInformation
+            // 
+            this.rbInformation.AutoSize = true;
+            this.rbInformation.Location = new System.Drawing.Point(26, 156);
+            this.rbInformation.Name = "rbInformation";
+            this.rbInformation.Size = new System.Drawing.Size(100, 21);
+            this.rbInformation.TabIndex = 1;
+            this.rbInformation.TabStop = true;
+            this.rbInformation.Text = "Information";
+            this.rbInformation.UseVisualStyleBackColor = true;
+            // 
+            // rbError
+            // 
+            this.rbError.AutoSize = true;
+            this.rbError.Location = new System.Drawing.Point(26, 56);
+            this.rbError.Name = "rbError";
+            this.rbError.Size = new System.Drawing.Size(56, 21);
+            this.rbError.TabIndex = 0;
+            this.rbError.TabStop = true;
+            this.rbError.Text = "Error";
+            this.rbError.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnShow.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
+            this.btnShow.ForeColor = System.Drawing.Color.Black;
+            this.btnShow.Location = new System.Drawing.Point(399, 307);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 30);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 391);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.gbOption2);
+            this.Controls.Add(this.gbOption1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "메시지 박스 보기";
+            this.gbOption1.ResumeLayout(false);
+            this.gbOption1.PerformLayout();
+            this.gbOption2.ResumeLayout(false);
+            this.gbOption2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbOption1;
+        private System.Windows.Forms.GroupBox gbOption2;
+        private System.Windows.Forms.RadioButton rbOk;
+        private System.Windows.Forms.RadioButton rbOkCancel;
+        private System.Windows.Forms.RadioButton rbYesNo;
+        private System.Windows.Forms.RadioButton rbQuestion;
+        private System.Windows.Forms.RadioButton rbInformation;
+        private System.Windows.Forms.RadioButton rbError;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ 03rd/sln_03/A_textView/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_textView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        string OrgStr = "";
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (TextCheck() == true)
+            {
+                this.lblResult.Text = OrgStr + this.txtEdit.Text;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OrgStr = this.lblResult.Text;
+        }
+        private void txtEdit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) //엔터키를 누를 때 
+            {
+                e.Handled = true; // 소리 없앰 
+                if (TextCheck() == true)
+                {
+                    this.lblResult.Text = OrgStr + this.txtEdit.Text;
+                }
+            }
+        }
+            private bool TextCheck()
+        {
+            if (this.txtEdit.Text != "") return true;
+            else
+            {
+                MessageBox.Show("텍스트를 입력하세요!", "알림",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtEdit.Focus();
+                return false;
+            }
+        }
+    }
+}
